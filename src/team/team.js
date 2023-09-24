730423b4a6e95495e38de3b11584b22a7224f336
Kae <kaehehehe@gmail.com>
2023-09-24 09:46:48 +0900

    feat: 매칭 기능 구현

--- src/team/team.js
+++ src/team/team.js
@@ -1,6 +1,7 @@
-import { getCrews } from '../crew/crew.js';
+import { getCrews, getCrewsCount } from '../crew/crew.js';
 import { renderCourseName } from '../shared/renderCourseName.js';
 import { removeElement } from '../shared/resetDom.js';
+
 
 export const renderMissionName = (mission) => {
   switch (mission) {
@@ -69,6 +70,74 @@ const clickConfirmButton = () => {
   });
 };
 
+const findCrewName = (course, index) => {
+  const crews = getCrews(course);
+
+  return crews[index].name;
+};
+
+const clickTeamMatching = (course, mission) => {
+  const matchTeamButton = document.getElementById('match-team-button');
+
+  matchTeamButton.addEventListener('click', () => {
+    const teamMemberCountInput = document.getElementById('team-member-count-input');
+    const crewsCount = getCrewsCount(course);
+    const teamMemberCount = teamMemberCountInput.value;
+    const teams = [];
+
+
+    const indexArray = Array.from({ length: crewsCount }, (_, idx) => idx);
+
+    // console.log(indexArray);
+
+    MissionUtils.Random.shuffle(indexArray);
+
+    while (indexArray.length > 0) {
+      const team = indexArray.splice(0, teamMemberCount);
+
+      let teamMember = [];
+
+      for (let i of team) {
+        teamMember.push(findCrewName(course, i));
+      }
+      teams.push(teamMember.toString());
+    }
+
+    renderResultSection(course, mission, teams);
+  });
+
+};
+
+const renderResultSection = (course, mission, teams) => {
+  const section = document.getElementById('team-section');
+
+  removeElement(section);
+
+  const elements = `
+  <h3>${renderCourseName(course)} ${renderMissionName(mission)}게임 조회</h3>
+    <p>팀이 매칭되었습니다.</p>
+    <ul id="team-match-result">
+    </ul>
+    <p>
+      팀을 재매칭 하시겠습니까?
+      <button id="rematch-team-button">재매칭</button>
+    </p>
+  `;
+
+  section.insertAdjacentHTML('beforeend', elements);
+
+  const teamMatchResult = document.getElementById('team-match-result');
+
+  const teamList = teams.reduce((prev, member) => prev + `<li>${member}</li>`, '');
+
+  teamMatchResult.insertAdjacentHTML('beforeend', teamList);
+
+
+}
+
+
+
+
 const renderDefaultMatchingSection = (course, mission) => {
   const section = document.querySelector('#team-section');
 
@@ -79,15 +148,21 @@ const renderDefaultMatchingSection = (course, mission) => {
           <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
           <form>
             <label>1팀당 인원 수</label>
-            <input id="team-member-count-input" type="number" />
-            <button id="match-team-button">팀 매칭</button>
+            <input
+              id="team-member-count-input"
+              type="number"
+              min="1"
+              max=${getCrewsCount(course)}
+              value="1"
+              />
+            <button id="match-team-button" type="button">팀 매칭</button>
           </form>
         </div>
         <h4>크루 목록</h4>
         <ul id="crew-list">
         </ul>
       </div>
-  `; 
+  `;
 
   section.insertAdjacentHTML('beforeend', elements);
 
@@ -97,4 +172,6 @@ const renderDefaultMatchingSection = (course, mission) => {
   const crewList = crews.reduce((prev, crew) => prev + `<li>${crew.name}</li>`, '');
 
   ul.insertAdjacentHTML('beforeend', crewList);
+
+  clickTeamMatching(course, mission);
 };
